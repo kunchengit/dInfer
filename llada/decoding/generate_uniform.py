c665d68d41575093ec412bece3569e57d4b0abc4
Da Zheng <zhengda1936@gmail.com>
2025-08-12 16:00:52 +0800

    move the code.

--- llada/decoding/generate_uniform.py
+++ llada/decoding/generate_uniform.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 import torch.nn.functional as F
 
-from utils import add_gumbel_noise
+from .utils import add_gumbel_noise
 
 def get_transfer_index(logits, temperature, remasking, mask_index, x, num_transfer_tokens, threshold=None):
     logits_with_noise = add_gumbel_noise(logits, temperature=temperature)
