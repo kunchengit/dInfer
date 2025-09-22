9b1f1debceb67a1048a6d318b418ecf237feb0ee
山汇 <yuxin.myx@antgroup.com>
2025-09-22 18:04:51 +0800

    PullRequest: 82 support unroll
    Merge branch 'myx_formalize_unroll of git@code.alipay.com:dulun.dl/Fast-dllm.git into formalize_api
    
    https://code.alipay.com/dulun.dl/Fast-dllm/pull_requests/82
    
    
    Reviewed-by: 旭新 <zhengda.zheng@antgroup.com>
    
    
    * support unroll
    * apply unroll on continuous
    * remove \ at end of line

--- python/dinfer/decoding/generate_uniform.py
+++ python/dinfer/decoding/generate_uniform.py
@@ -72,7 +72,7 @@ class BlockWiseDiffusionLLM:
     in the block before moving to the next block.
     This is a classifical dLLM decoding algorithm.
     """
-    def __init__(self, model, decoder, iterator_factory, early_stop=True, cache_factory=None):
+    def __init__(self, model, decoder, iterator_factory, early_stop=True, cache_factory=None, maximum_unroll=4, expected_tpf=8):
         self.model = model
         self.cache_factory = cache_factory
         self.decoder = decoder
@@ -80,6 +80,8 @@ class BlockWiseDiffusionLLM:
         self.num_forwards = 0
         self.cache_updates = 0
         self.early_stop = early_stop
+        self.maximum_unroll = maximum_unroll
+        self.expected_tpf = expected_tpf
 
     @ torch.no_grad()
     def _generate(self, prompt, gen_length=128, block_length=128):
@@ -94,31 +96,33 @@ class BlockWiseDiffusionLLM:
             self.decoder.block_init(block, block_id)
 
             while (block == self.decoder.mask_id).sum() > 0:
-                # Update KV-cache
-                if kv_cache is not None and kv_cache.require_update(iter_no, block_loc.start, block_loc.end):
-                    output = self.model(x.data, use_cache=True)
-                    self.num_forwards += 1
-                    # use the generated output to decode.
-                    self.decoder.decode(output.logits[:, block_loc.start:block_loc.end], block_loc.start, block_loc.end, x)
-                    # update KV-cache
-                    kv_cache.update(output.past_key_values)
-                    past_key_values, replace_position = kv_cache.get_key_values(block_loc.start, block_loc.end)
-                    self.cache_updates += 1
+                unroll_k = max(min((block == self.decoder.mask_id).sum()//self.expected_tpf, self.maximum_unroll), 1)
+                for unroll_i in range(unroll_k):
+                    # Update KV-cache
+                    if kv_cache is not None and kv_cache.require_update(iter_no, block_loc.start, block_loc.end):
+                        output = self.model(x.data, use_cache=True)
+                        self.num_forwards += 1
+                        # use the generated output to decode.
+                        self.decoder.decode(output.logits[:, block_loc.start:block_loc.end], block_loc.start, block_loc.end, x)
+                        # update KV-cache
+                        kv_cache.update(output.past_key_values)
+                        past_key_values, replace_position = kv_cache.get_key_values(block_loc.start, block_loc.end)
+                        self.cache_updates += 1
 
-                if kv_cache is None:
-                    logits = self.model(x.data).logits[:, block_loc.start:block_loc.end]
-                elif kv_cache.cache_type == 'prefix':
-                    logits = self.model(x[block_loc.start:], past_key_values=past_key_values, use_cache=True,
-                                        replace_position=replace_position).logits
-                    block_length = block_loc.end - block_loc.start
-                    logits = logits[:, :block_length]
-                else:
-                    # cache position is the position between current_block_start and current_block_end
-                    logits = self.model(block, past_key_values=past_key_values, use_cache=True,
-                                        replace_position=replace_position).logits
-                self.num_forwards += 1
-                self.decoder.decode(logits, block_loc.start, block_loc.end, x)
-                iter_no += 1
+                    if kv_cache is None:
+                        logits = self.model(x.data).logits[:, block_loc.start:block_loc.end]
+                    elif kv_cache.cache_type == 'prefix':
+                        logits = self.model(x[block_loc.start:], past_key_values=past_key_values, use_cache=True,
+                                            replace_position=replace_position).logits
+                        block_length = block_loc.end - block_loc.start
+                        logits = logits[:, :block_length]
+                    else:
+                        # cache position is the position between current_block_start and current_block_end
+                        logits = self.model(block, past_key_values=past_key_values, use_cache=True,
+                                            replace_position=replace_position).logits
+                    self.num_forwards += 1
+                    self.decoder.decode(logits, block_loc.start, block_loc.end, x)
+                    iter_no += 1
 
             if self.early_stop and torch.any(x[block_loc.start:block_loc.end] == self.decoder.eos_id):
                 # Find the first location of EOS and set all tokens after the location to EOS.
@@ -139,7 +143,8 @@ class BlockWiseDiffusionLLMCont(BlockWiseDiffusionLLM):
     in the block before moving to the next block.
     This is a classifical dLLM decoding algorithm.
     """
-    def __init__(self, model, decoder, iterator_factory, early_stop=True, cache_factory=None, cont_weight=0.3, cont_weight_init=0.1, cont_weight_growth=0.02, threshold_decay=0.02):
+    def __init__(self, model, decoder, iterator_factory, early_stop=True, cache_factory=None, maximum_unroll=4, expected_tpf=8,
+                cont_weight=0.3, cont_weight_init=0.1, cont_weight_growth=0.02, threshold_decay=0.02):
         self.model = model
         self.cache_factory = cache_factory
         self.decoder = decoder
@@ -155,6 +160,8 @@ class BlockWiseDiffusionLLMCont(BlockWiseDiffusionLLM):
         self.cont_weight_init = cont_weight_init
         self.cont_weight_growth = cont_weight_growth
         self.threshold_decay = threshold_decay
+        self.maximum_unroll = maximum_unroll
+        self.expected_tpf = expected_tpf
 
     @ torch.no_grad()
     def _generate(self, prompt, gen_length=128, block_length=128):
@@ -169,46 +176,49 @@ class BlockWiseDiffusionLLMCont(BlockWiseDiffusionLLM):
             self.decoder.block_init(block, block_id)
 
             while (block == self.decoder.mask_id).sum() > 0:
-                iter_cont_weight = min(self.cont_weight_init+self.cont_weight_growth*iter_no, self.cont_weight)
-                iter_threshold = max(1-iter_no*self.threshold_decay, self.decoder.threshold)
-                # Update KV-cache
-                if kv_cache is not None and kv_cache.require_update(iter_no, block_loc.start, block_loc.end):
-                    output = self.model(inputs_embeds=inputs_embeds, use_cache=True)
+                unroll_k = max(min((block == self.decoder.mask_id).sum()//self.expected_tpf, self.maximum_unroll), 1)
+                for unroll_i in range(unroll_k):
+
+                    iter_cont_weight = min(self.cont_weight_init+self.cont_weight_growth*iter_no, self.cont_weight)
+                    iter_threshold = max(1-iter_no*self.threshold_decay, self.decoder.threshold)
+                    # Update KV-cache
+                    if kv_cache is not None and kv_cache.require_update(iter_no, block_loc.start, block_loc.end):
+                        output = self.model(inputs_embeds=inputs_embeds, use_cache=True)
+                        self.num_forwards += 1
+                        # use the generated output to decode.
+                        self.decoder.decode(output.logits[:, block_loc.start:block_loc.end], block_loc.start, block_loc.end, x, iter_threshold)
+                        # update KV-cache
+                        mask_index = (x.data == self.decoder.mask_id)
+                        inputs_embeds = self.h2e(x.data, mask_index, output.logits, iter_cont_weight)
+                        kv_cache.update(output.past_key_values)
+                        past_key_values, replace_position = kv_cache.get_key_values(block_loc.start, block_loc.end)
+                        self.cache_updates += 1
+                        iter_no += 1
+
+                    iter_cont_weight = min(self.cont_weight_init+self.cont_weight_growth*iter_no, self.cont_weight)
+                    iter_threshold = max(1-iter_no*self.threshold_decay, self.decoder.threshold)
+                    if kv_cache is None:
+                        logits = self.model(inputs_embeds=inputs_embeds).logits
+                        self.decoder.decode(logits[:, block_loc.start:block_loc.end], block_loc.start, block_loc.end, x, iter_threshold)
+                        mask_index = (x.data == self.decoder.mask_id)
+                        inputs_embeds = self.h2e(x.data, mask_index, logits, iter_cont_weight)
+                    elif kv_cache.cache_type == 'prefix':
+                        logits = self.model(inputs_embeds=inputs_embeds[:, block_loc.start:], past_key_values=past_key_values, use_cache=True,
+                                            replace_position=replace_position).logits
+                        block_length = block_loc.end - block_loc.start
+                        self.decoder.decode(logits[:, :block_length], block_loc.start, block_loc.end, x, iter_threshold)
+                        mask_index = (x.data[:, block_loc.start:] == self.decoder.mask_id)
+                        inputs_embeds[:, block_loc.start:] = self.h2e(x.data[:, block_loc.start:], mask_index, logits, iter_cont_weight)
+                    else:
+                        # cache position is the position between current_block_start and current_block_end
+                        logits = self.model(inputs_embeds=inputs_embeds[:, block_loc.start:block_loc.end], past_key_values=past_key_values, use_cache=True,
+                                            replace_position=replace_position).logits
+                        self.decoder.decode(logits, block_loc.start, block_loc.end, x, iter_threshold)
+                        mask_index = (x.data[:, block_loc.start:block_loc.end] == self.decoder.mask_id)
+                        inputs_embeds[:, block_loc.start:block_loc.end] = self.h2e(x.data[:, block_loc.start:block_loc.end], mask_index, logits, iter_cont_weight)
                     self.num_forwards += 1
-                    # use the generated output to decode.
-                    self.decoder.decode(output.logits[:, block_loc.start:block_loc.end], block_loc.start, block_loc.end, x, iter_threshold)
-                    # update KV-cache
-                    mask_index = (x.data == self.decoder.mask_id)
-                    inputs_embeds = self.h2e(x.data, mask_index, output.logits, iter_cont_weight)
-                    kv_cache.update(output.past_key_values)
-                    past_key_values, replace_position = kv_cache.get_key_values(block_loc.start, block_loc.end)
-                    self.cache_updates += 1
+
                     iter_no += 1
-
-                iter_cont_weight = min(self.cont_weight_init+self.cont_weight_growth*iter_no, self.cont_weight)
-                iter_threshold = max(1-iter_no*self.threshold_decay, self.decoder.threshold)
-                if kv_cache is None:
-                    logits = self.model(inputs_embeds=inputs_embeds).logits
-                    self.decoder.decode(logits[:, block_loc.start:block_loc.end], block_loc.start, block_loc.end, x, iter_threshold)
-                    mask_index = (x.data == self.decoder.mask_id)
-                    inputs_embeds = self.h2e(x.data, mask_index, logits, iter_cont_weight)
-                elif kv_cache.cache_type == 'prefix':
-                    logits = self.model(inputs_embeds=inputs_embeds[:, block_loc.start:], past_key_values=past_key_values, use_cache=True,
-                                        replace_position=replace_position).logits
-                    block_length = block_loc.end - block_loc.start
-                    self.decoder.decode(logits[:, :block_length], block_loc.start, block_loc.end, x, iter_threshold)
-                    mask_index = (x.data[:, block_loc.start:] == self.decoder.mask_id)
-                    inputs_embeds[:, block_loc.start:] = self.h2e(x.data[:, block_loc.start:], mask_index, logits, iter_cont_weight)
-                else:
-                    # cache position is the position between current_block_start and current_block_end
-                    logits = self.model(inputs_embeds=inputs_embeds[:, block_loc.start:block_loc.end], past_key_values=past_key_values, use_cache=True,
-                                        replace_position=replace_position).logits
-                    self.decoder.decode(logits, block_loc.start, block_loc.end, x, iter_threshold)
-                    mask_index = (x.data[:, block_loc.start:block_loc.end] == self.decoder.mask_id)
-                    inputs_embeds[:, block_loc.start:block_loc.end] = self.h2e(x.data[:, block_loc.start:block_loc.end], mask_index, logits, iter_cont_weight)
-                self.num_forwards += 1
-
-                iter_no += 1
 
             if self.early_stop and torch.any(x[block_loc.start:block_loc.end] == self.decoder.eos_id):
                 # Find the first location of EOS and set all tokens after the location to EOS.
